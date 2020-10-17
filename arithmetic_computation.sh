ed549aea82dc9c05284623f0d411890decafbc16
AkashSuchak <suchak.akash@gmail.com>
2020-10-17 13:19:18 +0530

    Result of all Expressions Store in Dictionary

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,17 +1,31 @@
 #! /bin/bash 
 
 #Author : Akash Suchak
-#Compute a % b + c
+#Store the results in a Dictionary for every Computation
 
 #User Input assign to variables
 read -p "Enter Value of A : " a
 read -p "Enter Value of B : " b
 read -p "Enter Value of C : " c
 
+#Declaring Dictionary
+declare -A results_expr
+
 #Condition to checck User Inputs are integer or not then we calculate
 if [[ $a ]] && [[ $b ]] && [[ $c ]] && [ $a -eq $a 2>/dev/null ] && [ $b -eq $b 2>/dev/null ] && [ $c -eq $c 2>/dev/null ]; then
-	result=`awk -v a=$a -v b=$b -v c=$c 'BEGIN{result=(a % b + c); print result}' `
-	echo "(a % b + c ) : "$result
+	result1=`awk -v a=$a -v b=$b -v c=$c 'BEGIN{result=(a + b * c); print result}' `
+	results_expr[expr1]=$result1
+
+	result2=`awk -v a=$a -v b=$b -v c=$c 'BEGIN{result=(a * b + c); print result}' `
+	results_expr[expr2]=$result2
+
+	result3=`awk -v a=$a -v b=$b -v c=$c 'BEGIN{result=(c + a / b); print result}' `
+	results_expr[expr3]=$result3
+
+	result4=`awk -v a=$a -v b=$b -v c=$c 'BEGIN{result=(a % b + c); print result}' `
+	results_expr[expr4]=$result4
+
 else
      echo "Wrong Input!! Enter Inputs Only."
 fi
+echo "Result of All Expressions " ${results_expr[@]}
