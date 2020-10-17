98b4c432a33554d30907d75e9daeb1f19d7d17e7
AkashSuchak <suchak.akash@gmail.com>
2020-10-17 12:22:31 +0530

    Expression3 (c + a / b)

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /bin/bash 
 
 #Author : Akash Suchak
-#Compute a * b + c
+#Compute c + a / b
 
 #User Input assign to variables
 read -p "Enter Value of A : " a
@@ -10,8 +10,8 @@ read -p "Enter Value of C : " c
 
 #Condition to checck User Inputs are integer or not then we calculate
 if [[ $a ]] && [[ $b ]] && [[ $c ]] && [ $a -eq $a 2>/dev/null ] && [ $b -eq $b 2>/dev/null ] && [ $c -eq $c 2>/dev/null ]; then
-	result=$(( a * b + c ))
-	echo "(a * b + c ) : "$result
+	result=`awk -v a=$a -v b=$b -v c=$c 'BEGIN{result=(c + a / b); print result}' `
+	echo "(c + a / b ) : "$result
 else
      echo "Wrong Input!! Enter Inputs Only."
 fi
