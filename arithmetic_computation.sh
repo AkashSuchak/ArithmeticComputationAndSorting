037c5275f71acebdb88a180a22063209e445064c
AkashSuchak <suchak.akash@gmail.com>
2020-10-17 11:17:02 +0530

    Expression1-(a + b * c)

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Welcome Message
+#Compute a + b * c
 
 #User Input assign to variables
 read -p "Enter Value of A : " a
 read -p "Enter Value of B : " b
 read -p "Enter Value of C : " c
+
+#Condition to checck User Inputs are integer or not then we calculate
+if [[ $a ]] && [[ $b ]] && [[ $c ]] && [ $a -eq $a 2>/dev/null ] && [ $b -eq $b 2>/dev/null ] && [ $c -eq $c 2>/dev/null ]; then
+	result=$(( a + b * c ))
+	echo $result
+else
+     echo "Wrong Input!! Enter Inputs Only."
+fi
